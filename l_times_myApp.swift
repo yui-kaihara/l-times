9e50d7df2aa9e5b019d47bfacd8bd7d0f2a5d228
y-kaihara <jieyikaiyuan@gmail.com>
2025-12-24 17:49:39 +0900

    initial commit

---
+++ l_times_myApp.swift
@@ -0,0 +1,17 @@
+//
+//  l_times_myApp.swift
+//  l-times-my
+//
+//  Created by apple on 2025/12/24.
+//
+
+import SwiftUI
+
+@main
+struct l_times_myApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
